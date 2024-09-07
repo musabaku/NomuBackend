3a6c204c4f212ef1fdc676b1b26da4b4c4fb7ca9
Musab Ahmed Khan Umair <110651555+musabaku@users.noreply.github.com>
2024-09-07 21:01:54 +0300

    Update Docs.cs

--- Nomu-Backend-master/NomuBackend/NomuBackend/Model/Docs.cs
+++ Nomu-Backend-master/NomuBackend/NomuBackend/Model/Docs.cs
@@ -1,1 +1,9 @@
-c
+public class Docs
+{
+    public string DocId { get; set; }
+    public string DocType { get; set; }
+    public string DocContent { get; set; }
+    public string ProductCatalogue { get; set; }
+    public string StaffInfo { get; set; }
+    public string ContactInfo { get; set; }
+}
