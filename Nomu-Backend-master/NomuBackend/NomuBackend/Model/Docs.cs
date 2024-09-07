71d0d615f88729e191428ee127c551d9c504b708
Musab Ahmed Khan Umair <110651555+musabaku@users.noreply.github.com>
2024-09-07 21:04:06 +0300

    Update Docs.cs

--- Nomu-Backend-master/NomuBackend/NomuBackend/Model/Docs.cs
+++ Nomu-Backend-master/NomuBackend/NomuBackend/Model/Docs.cs
@@ -7,3 +7,33 @@ public class Docs
     public string StaffInfo { get; set; }
     public string ContactInfo { get; set; }
 }
+// Below are CRUD operations that can be done using above fields
+
+// public void InsertDoc(Docs doc)
+// {
+//     var collection = _database.GetCollection<Docs>("Docs");
+//     collection.InsertOne(doc);
+// }
+// public Docs GetDocById(string docId)
+// {
+//     var collection = _database.GetCollection<Docs>("Docs");
+//     return collection.Find(d => d.DocId == docId).FirstOrDefault();
+// }
+// public void UpdateDoc(string docId, Docs updatedDoc)
+// {
+//     var collection = _database.GetCollection<Docs>("Docs");
+//     var filter = Builders<Docs>.Filter.Eq(d => d.DocId, docId);
+//     var update = Builders<Docs>.Update
+//                     .Set(d => d.DocType, updatedDoc.DocType)
+//                     .Set(d => d.DocContent, updatedDoc.DocContent)
+//                     .Set(d => d.ProductCatalogue, updatedDoc.ProductCatalogue)
+//                     .Set(d => d.StaffInfo, updatedDoc.StaffInfo)
+//                     .Set(d => d.ContactInfo, updatedDoc.ContactInfo);
+
+//     collection.UpdateOne(filter, update);
+// }
+// public void DeleteDoc(string docId)
+// {
+//     var collection = _database.GetCollection<Docs>("Docs");
+//     collection.DeleteOne(d => d.DocId == docId);
+// }
